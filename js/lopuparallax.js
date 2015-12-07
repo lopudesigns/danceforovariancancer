8df2b374a907025c6326e2be0626db6c0d4f2222
lopu <lopu@p3plcpnl0562.prod.phx3.secureserver.net>
2015-12-07 08:44:36 -0700

    test yo

---
+++ js/lopuparallax.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+
+	//gets height
+	var pContainerHeight = $('#heightid').outerHeight(true);
+	console.log(pContainerHeight);
+	//gives div that height
+	$('.kujoimgpos').css({'height' : pContainerHeight + 'px'});
+
+	$(window).scroll(function(){
+				
+		var scrollStart = $('#imgstart').offset().top - $(window).height();
+		
+		console.log(scrollStart);
+
+		var wScroll = $(this).scrollTop();
+		
+		console.log(wScroll);
+		
+		var scrollBottom = $(window).scrollTop() + $(window).height();
+		
+		console.log(scrollBottom);
+		
+		if (wScroll >= scrollStart) {
+
+		$('.kujoimgpos').css({
+			  'background-position' : '0 -'+(60 + ((wScroll - scrollStart) *.2)) +'px'
+		});
+	  }
+	});
+
+});
